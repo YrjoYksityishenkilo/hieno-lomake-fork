0dbc54a3e2f76127926db3b327ffca1e737b1387
Joona Nikitin <joona.nikitin@edu.mayk.fi>
2025-03-21 11:30:48 +0200

    aiempi

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def root():
 @app.route("/vastaus")
 def vastaus():
     uusi_nimi = request.args['nimi']
-    with open("kaikki_nimet.txt", "a") as nimitiedosto:
-        nimitiedosto.write(uusi_nimi + "/n")
-    kaikki_nimet = open("kaikki_nimet.txt").read()
-    lauseke = request.args['lauseke']
+    #with open("kaikki_nimet.txt", "a") as nimitiedosto:
+     #   nimitiedosto.write(uusi_nimi + "/n")
+   # kaikki_nimet = open("kaikki_nimet.txt").read()
+    #lauseke = request.args['lauseke']
     return render_template('vastaus.html', nimi=request.args['nimi'])
 
 if __name__=='__main__':
